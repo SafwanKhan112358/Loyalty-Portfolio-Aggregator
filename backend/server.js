a489b3df7845a515e5627ed97f3723364998d2a4
Safwan Khan <safwan.khan0305@gmail.com>
2026-07-23 14:30:13 -0400

    connecting backend to database

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,17 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req,res)=>{
-    res.send("Route works!");
+app.get("/", async (req,res)=>{
+    //res.send("Route works!");
+    const result = await pool.query("SELECT * FROM Testing");
+    res.json(result.rows);
 });
 
 const PORT = process.env.PORT || 4000;
