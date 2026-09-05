c981d32acc677d2f8b836894f9f7d2e1d78a2bd9
Safwan Khan <safwan.khan0305@gmail.com>
2026-09-05 01:37:31 -0400

    finished signup and login frontend parts

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,73 @@
+import { useState } from "react";
+import { apiUrl } from "../api";
+
 function Login(){
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [token, setToken] = useState("");
+    const [error, setError] = useState("");
+
+    async function loginHandler(e){
+        e.preventDefault();
+
+        try{
+            const result = await fetch(`${apiUrl}/login`,{
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({email, password})
+            });
+
+            const data = await result.json();
+
+            if(!result.ok){
+                setError(data.message);
+                setToken("");
+                return;
+            }
+
+            setToken(data.token);
+            setError("");
+        }catch(e){
+            setError("Something went wrong. Please try again.");
+            setToken("");
+        }
+    }
+
     return(
-        <h1>Login</h1>
-    )
+        <>
+            <h1>Login</h1>
+
+            <form onSubmit={loginHandler}>
+                <h2>Email</h2>
+                <input 
+                    value={email}
+                    onChange={
+                        (e) => {
+                            setEmail(e.target.value);
+                        }
+                    }
+                />
+
+                <h2>Password</h2>
+                <input 
+                    type="password"
+                    value={password}
+                    onChange={
+                        (e) =>{
+                            setPassword(e.target.value);
+                        }
+                    }
+                />
+
+                <button>Log in</button>
+
+            </form>
+
+            {token && <h3>Login successful</h3>}
+            {error && <h3>{error}</h3>}
+        </>
+    );
 }
 
 export default Login;
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,6 +1,90 @@
+import {apiUrl} from "../api";
+import { use, useState } from "react";
+import {useNavigate} from "react-router-dom";
+
 function Signup(){
+    
+    const navigate = useNavigate();
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+    async function signupHandler(e){
+        e.preventDefault();
+        try{
+            const result = await fetch(`${apiUrl}/signup`,{
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({name, email, password})
+            });
+
+            const data = await result.json();
+
+            if (!result.ok){
+                setError(data.message);
+                setSuccess("");
+                return;
+            }
+
+            setSuccess("Sign up successful");
+            setError("");
+
+            setTimeout(()=>{
+                navigate("/login");
+            },3000);
+
+            
+        }catch(e){
+            setError("Something went wrong. Please try again");
+            setSuccess("");
+        }
+    }
     return(
+    <>
         <h1>Signup</h1>
+
+        <form onSubmit={signupHandler}>
+            <h2>Name</h2>
+            <input 
+                value={name}
+                onChange={
+                    (e) => {
+                        setName(e.target.value);
+                    }
+                }
+            />
+
+            <h2>Email</h2>
+            <input
+                value={email}
+                onChange={
+                    (e) => {
+                        setEmail(e.target.value);
+                    }
+                }
+            />
+
+            <h2>Password</h2>
+            <input 
+                type="password"
+                value={password}
+                onChange={
+                    (e)=>{
+                        setPassword(e.target.value);
+                    }
+                }
+            />
+
+            <button>Sign up</button>
+
+        </form>
+
+        {success && <h3>{success}</h3>}
+        {error && <h3>{error}</h3>}
+    </>
     );
 }
 
