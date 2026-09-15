3a36b9bf9d152de276f0d87a43fc582595adc45b
Safwan Khan <safwan.khan0305@gmail.com>
2026-09-14 23:02:52 -0400

    wrote a helper function

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,11 @@
 export const apiUrl = import.meta.env.VITE_API_URL;
+
+export function authFetch(url, options = {}, token){
+    return fetch(url,{
+        ...options,
+        headers: {
+            ...options.headers,
+            Authorization: `Bearer ${token}`
+        }
+    });
+}
